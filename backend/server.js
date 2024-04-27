f8bca520950963faaba5dc4d9dc5c5d5926eec21
Sreejita Dutta <sreejitadutta23@gmail.com>
2024-04-28 00:01:04 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req,res) => {
     //root route http.//localhost:5000/
     res.send("Hello World!");
 });
 
+app.use("/api/auth", authRoutes)
 
+app.get("api/auth/signup", (req,res) => {
+    console.log("signup route");
+});
 
-app.listen(5000,() => console.log("Server Running on port 5000"));
+app.get("api/auth/login", (req,res) => {
+    console.log("login route");
+});
+
+app.get("api/auth/logout", (req,res) => {
+    console.log("logout route");
+});
+
+app.listen(PORT,() => console.log(`Server Running on port ${PORT}`));
 
