676b2769b7a3dbba5ecddd7e73cdfc8f5e04897b
Sreejita Dutta <sreejitadutta23@gmail.com>
2024-04-29 16:34:14 +0530

    setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,23 @@
 
-export const signup = (req,res) => {
-    console.log("signup");
+export const signup = async (req,res) => {
+
+    try {
+        const {fullName,username,password,confirmPassword,gender} = req.body;
+    } catch (error) {
+        
+    }
+
+    // res.send("signup");
+    // console.log("signup");
 }
 
 export const login = (req,res) => {
+    res.send("login");
+
     console.log("login");
 }
 
 export const logout = (req,res) => {
+    res.send("logout");
     console.log("logout");
 }
