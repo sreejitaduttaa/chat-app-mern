7826ed682b3a2d4c31b483ec7cca9861c6aefa26
Sreejita Dutta <sreejitadutta23@gmail.com>
2024-05-01 18:52:33 +0530

    jwt

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 
 export const signup = async (req,res) => {
@@ -19,10 +20,9 @@ export const signup = async (req,res) => {
         //HASH PASSOW0RD HERE
         const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
-
   
-        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username={username}`
-        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username={username}`
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
 
         const newUser = new User({
             fullName,
@@ -32,14 +32,19 @@ export const signup = async (req,res) => {
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
 
-        await newUser.save();
+        if(newUser){
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            });
+        } else {
+            res.status(400).json({error: "Invalid user data"});
+        }    
 
     } catch (error) {
         console.log("Error in signup controller", error.message);
