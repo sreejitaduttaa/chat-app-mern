dd9a939a1f1d52acfcac98bca40cd40fd89ccfc9
Sreejita Dutta <sreejitadutta23@gmail.com>
2024-05-02 21:17:55 +0530

    protect route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
  
 const app = express();
@@ -10,14 +12,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with JSON payLoads(from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
-// app.get("/", (req,res) => {
-    //root route http.//localhost:5000/
-//     res.send("Hello World!");
-// });
-
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT,() => {
     connectToMongoDB();
